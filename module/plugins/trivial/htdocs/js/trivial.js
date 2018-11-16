53c98670e341eac1675c12b09a1dc1c4ec5a128c
Pablo Álvarez Córdoba <pabloalvarezcordoba@protonmail.ch>
2018-11-16 09:44:01 +0100

    up

--- module/plugins/trivial/htdocs/js/trivial.js
+++ module/plugins/trivial/htdocs/js/trivial.js
@@ -1,287 +1,273 @@
 // layout = window.cy.makeLayout({'name': 'cose'})
 // layout.options.eles = window.cy.elements();
 // layout.run()
-
-
-
 var cy_status;
 
-var ctxmenu_commands_all = [
-  {
+var ctxmenu_commands_all = [{
     content: 'Search',
-    select: function(){
-      cy_status = cy.nodes();
-      trivial_search(this.data('id'))
+    select: function() {
+        cy_status = cy.nodes();
+        trivial_search(this.data('id'))
     }
-  },{
+}, {
     content: 'Expand',
-    select: function(){
-      cy_status = cy.nodes();
-      trivial_expand(this.data('id'))
+    select: function() {
+        cy_status = cy.nodes();
+        trivial_expand(this.data('id'))
     }
-  },{
+}, {
     content: 'View',
-    select: function(){
-      var url = "/cpe/" +  this.data('id');
-      var win = window.open(url, '_blank');
-      win.focus();
+    select: function() {
+        var url = "/cpe/" + this.data('id');
+        var win = window.open(url, '_blank');
+        win.focus();
     }
-  }
-]
-
+}]
 
 var ctxmenu_commands_mikrotik = ctxmenu_commands_all.slice()
 
 ctxmenu_commands_mikrotik.push({
     content: 'Winbox',
-    select: function(){
-      top.location.href= "winbox://" + username + "@" +  this.data('address') + ':8291';
+    select: function() {
+        top.location.href = "winbox://" + username + "@" + this.data('address') + ':8291';
     }
 });
 
 ctxmenu_commands_mikrotik.push({
-  content: 'SSH',
-  select: function(){
-    top.location.href= "krillssh://" + username + "@" +  this.data('address') + ':22';
-  }
+    content: 'SSH',
+    select: function() {
+        top.location.href = "krillssh://" + username + "@" + this.data('address') + ':22';
+    }
 });
 
 
 var ctxmenu_commands_access = ctxmenu_commands_all.slice()
 
-ctxmenu_commands_access.push(  {
+ctxmenu_commands_access.push({
     content: 'Enter the Matrix',
-    select: function(){
-      var url = "/matrix/?search=reg:" +  this.data('id');
-      var win = window.open(url, '_blank');
-      win.focus();
+    select: function() {
+        var url = "/matrix/?search=reg:" + this.data('id');
+        var win = window.open(url, '_blank');
+        win.focus();
     }
 });
 
 
 var ctxmenu_commands_wimax = ctxmenu_commands_all.slice()
 
-ctxmenu_commands_wimax.push(  {
+ctxmenu_commands_wimax.push({
     content: 'Web',
-    select: function(){
-      var url = "http://" + this.data('address') + '.' + window.location.host.split('.')[0] + '.phicus.net';
-      var win = window.open(url, '_blank');
-      win.focus();
+    select: function() {
+        var url = "http://" + this.data('address') + '.' + window.location.host.split('.')[0] + '.phicus.net';
+        var win = window.open(url, '_blank');
+        win.focus();
     }
 });
 
-ctxmenu_commands_wimax.push(  {
+ctxmenu_commands_wimax.push({
     content: 'Enter the Matrix',
-    select: function(){
-      var url = "/matrix?search=reg:" +  this.data('id');
-      var win = window.open(url, '_blank');
-      win.focus();
+    select: function() {
+        var url = "/matrix?search=reg:" + this.data('id');
+        var win = window.open(url, '_blank');
+        win.focus();
     }
 });
 
-var ctxmenu_commands_cpe = [
-  {
+var ctxmenu_commands_cpe = [{
     content: 'View',
-    select: function(){
-      var url = "/cpe/" +  this.data('id');
-      var win = window.open(url, '_blank');
-      win.focus();
+    select: function() {
+        var url = "/cpe/" + this.data('id');
+        var win = window.open(url, '_blank');
+        win.focus();
     }
-  }
-]
+}]
 
 
 ///Layouts
 var LAYOUT1 = {
-  name: 'cose-bilkent',
-  stop: function(){
-    console.log("cy::stop []");
-    window.cy.nodes().lock();
-  },
-  randomize: true,
-  gravityRangeCompound: 0.25,
-  nodeDimensionsIncludeLabels: false,
-  nodeRepulsion: 1000 * 1000,
-  tile: true
+    name: 'cose-bilkent',
+    stop: function() {
+        console.log("cy::stop []");
+        window.cy.nodes().lock();
+    },
+    randomize: true,
+    gravityRangeCompound: 0.25,
+    nodeDimensionsIncludeLabels: false,
+    nodeRepulsion: 1000 * 1000,
+    tile: true
 }
 
-var LAYOUT2 = {
-  name: 'preset'
-}
+// LAYAOUT2 is unused
+// var LAYOUT2 = {
+//     name: 'preset'
+// }
 
 function trivial_expand(txt) {
-  $.getJSON( "trivial.json?search=" + txt, function( data ) {
-    console.log(data)
-    window.cy.add(data);
-  });
+    $.getJSON("trivial.json?search=" + txt, function(data) {
+        console.log(data);
+        window.cy.add(data);
+    });
 }
 
 function trivial_init(data) {
-  var cy = cytoscape({
-    container: document.getElementById('trivial'),
-
-    ready: function(){
-      console.log("cy::ready []");
-      window.cy = this;
-    },
-
-    boxSelectionEnabled: true,
-    maxZoom: 2,
-    minZoom: 0.125,
-
-    style: TRIVIAL_STYLE,
-
-    elements: data,
-
-    layout: LAYOUT1
-  });
+    var cy = cytoscape({
+        container: document.getElementById('trivial'),
+        ready: function() {
+            console.log("cy::ready []");
+            window.cy = this;
+        },
+        boxSelectionEnabled: true,
+        maxZoom: 2,
+        minZoom: 0.125,
+        style: TRIVIAL_STYLE,
+        elements: data,
+        layout: LAYOUT1
+    });
 }
 
 function trivial_search(txt) {
     $('#search').val(txt);
-    history.pushState('trivial:'+txt, 'Trivial: '+txt, '/trivial?search='+txt);
-    $.getJSON( "trivial.json?search=" + txt, function( data ) {
-      trivial_init(data);
+    history.pushState('trivial:' + txt, 'Trivial: ' + txt, '/trivial?search=' + txt);
+    $.getJSON("trivial.json?search=" + txt, function(data) {
+        trivial_init(data);
 
-      window.cy.cxtmenu({
-        selector: 'node',
-        commands: function(e){
-            console.log(this)
+        window.cy.cxtmenu({
+            selector: 'node',
+            commands: function(e) {
+                console.log(this)
 
-            if (e.data()['tech'] == "wimax") {
-              return ctxmenu_commands_wimax;
+                if (e.data()['tech'] == "wimax") {
+                    return ctxmenu_commands_wimax;
+                }
+
+                if (e.data()['model'].search('Mikrotik') == 0) {
+                    return ctxmenu_commands_mikrotik;
+                }
+
+                return ctxmenu_commands_all;
             }
+        });
 
-            if (e.data()['model'].search('Mikrotik') == 0) {
-              return ctxmenu_commands_mikrotik;
-            }
+        // window.cy.cxtmenu({
+        //   selector: 'node.ap',
+        //   commands: ctxmenu_commands_wimax
+        // });
 
-            return ctxmenu_commands_all;
-        }
-      });
-
-      // window.cy.cxtmenu({
-      //   selector: 'node.ap',
-      //   commands: ctxmenu_commands_wimax
-      // });
-
-      window.cy.nodes().bind("mouseover", function(event){
-        var node = event.target;
-
-        //$('#resumen').load('/cpe/quickservices/' + node.data().id )
-
-      });
-  });
+        window.cy.nodes().bind("mouseover", function(event) {
+            var node = event.target;
+            //$('#resumen').load('/cpe/quickservices/' + node.data().id )
+        });
+    });
 }
 
 
-
-
-
-$(function(){
-  trivial_search( $('#txtSearch').val() );
+$(function() {
+    trivial_search($('#txtSearch').val());
 })
 
 
-
 function savePosition() {
-  data = {}
+    data = {}
 
-  $.each(window.cy.nodes(), function(k,node){
-    data[ node.data().id ] = {
-      'position': node.position()
-    };
-  });
+    $.each(window.cy.nodes(), function(k, node) {
+        data[node.data().id] = {
+            'position': node.position()
+        };
+    });
 
-  localStorage.setItem('trivial', JSON.stringify(data));
+    localStorage.setItem('trivial', JSON.stringify(data));
 }
 
 function loadPosition() {
-  var loadData = JSON.parse(localStorage.getItem('trivial'));
+    var loadData = JSON.parse(localStorage.getItem('trivial'));
 
-  if(loadData) {
+    if (loadData) {
 
-    $.each(loadData, function(k,v){
-      //console.log(v);
-      ele = window.cy.getElementById(k);
-      ele.position(v.position)
-    })
+        $.each(loadData, function(k, v) {
+            //console.log(v);
+            ele = window.cy.getElementById(k);
+            ele.position(v.position)
+        })
 
-  }
+    }
 
-  // $.each(data.nodes, function(k,v){
-  //   _id = v.data.id;
-  //   if (_id in loadData) {
-  //     console.log(_id)
-  //     position = saveData[_id].position
-  //     v['position'] = saveData[_id].position
-  //   }
-  // });
+    // $.each(data.nodes, function(k,v){
+    //   _id = v.data.id;
+    //   if (_id in loadData) {
+    //     console.log(_id)
+    //     position = saveData[_id].position
+    //     v['position'] = saveData[_id].position
+    //   }
+    // });
 }
 
 
 $('#load-position').hide();
 $('#save-position').hide();
 
-function workMode(){
-  $('#load-position').show();
-  $('#save-position').show();
-  $('#view-mode').show();
-  $('#work-mode').hide();
-  window.cy.nodes().unlock();
+function workMode() {
+    $('#load-position').show();
+    $('#save-position').show();
+    $('#view-mode').show();
+    $('#work-mode').hide();
+    window.cy.nodes().unlock();
 }
 
-function viewMode(){
-  $('#load-position').hide();
-  $('#save-position').hide();
-  $('#view-mode').hide();
-  $('#work-mode').show();
-  window.cy.nodes().lock();
+function viewMode() {
+    $('#load-position').hide();
+    $('#save-position').hide();
+    $('#view-mode').hide();
+    $('#work-mode').show();
+    window.cy.nodes().lock();
 }
 
-$('#work-mode').on('click', function(){
-  workMode();
+$('#work-mode').on('click', function() {
+    workMode();
 });
 
-$('#view-mode').on('click', function(){
-  viewMode();
+$('#view-mode').on('click', function() {
+    viewMode();
+});
+
+$('#save-position').on('click', function() {
+    console.log("savePosition []")
+    savePosition();
+});
+
+$('#load-position').on('click', function() {
+    console.log("loadPosition []")
+    loadPosition();
 });
 
 
-$('#save-position').on('click', function(){
-  console.log("savePosition []")
-  savePosition();
-});
+$('#play').on('click', function() {
+    //
 
-$('#load-position').on('click', function(){
-  console.log("loadPosition []")
-  loadPosition();
-});
-
-
-$('#play').on('click', function(){
-//
-
-  var b = window.cy
-
-  .nodes().animate({
-    position: { x: 0, y: 0 }
-  })
-
-  .delay(1000)
-
-  .animate({
-      pan: { x: 0, y: 0 }
-  });
+    var b = window.cy
+        .nodes().animate({
+            position: {
+                x: 0,
+                y: 0
+            }
+        })
+        .delay(1000)
+        .animate({
+            pan: {
+                x: 0,
+                y: 0
+            }
+        });
 
     // a.animation().play().promise().then(function () {
     //     b.animation().play();
     // });
-//--
+    //--
 });
 
 $(window).on('popstate', function(event) {
-  if (cy_status) {
-    cy.add(cy_status);
-  }
- });
+    if (cy_status) {
+        cy.add(cy_status);
+        cy.zoom(0.07);
+        $("#load-position").click();
+    }
+});
