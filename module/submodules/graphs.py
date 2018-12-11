ec72f137932450efaf6e343b0a62d8c823797d2e
johnbarneta <jgomez@phicus.es>
2018-12-11 12:14:17 +0100

    fix

--- module/submodules/graphs.py
+++ module/submodules/graphs.py
@@ -16,15 +16,15 @@ class GraphsMetaModule(MetaModule):
     _custom_log = "You should configure the module 'graphite' in your broker and the module 'ui-graphite' in webui.cfg file to be able to display graphs."
 
     def get_graph_uris(self, elt, graphstart=None, graphend=None, duration=None, source='detail'):
-        ''' Aggregate the get_graph_uris of all the submodules. 
-            The source parameter defines the source of the calling: 
-            Are we displaying graphs for the element detail page (detail), 
+        ''' Aggregate the get_graph_uris of all the submodules.
+            The source parameter defines the source of the calling:
+            Are we displaying graphs for the element detail page (detail),
             or a widget in the dashboard (dashboard) ?
 
             If duration is not None, we return the graph from now minus duration until now.
             But we also round "now" to the minute, to allow the cache to work and reduce
             network load.
-            
+
             Else we use graphstart and graphend to call the module get_graphs_uri
         '''
         uris = []
@@ -35,11 +35,11 @@ class GraphsMetaModule(MetaModule):
                 graphend = (int(time.time()) / 60) * 60
                 graphstart = graphend - duration
                 uris.extend(mod.get_graph_uris(elt, graphstart, graphend, source))
-                
+
             logger.debug("[WebUI] Got graphs: %s", uris)
 
         for uri in uris:
-            # uri['img_src'] = '/graph?url=' + urllib.quote(uri['img_src'])
-            uri['img_src'] = '/graph?' + urlparse(uri['img_src']).query
+            uri['img_src'] = '/graph?url=' + urllib.quote(uri['img_src'])
+            #uri['img_src'] = '/graph?' + urlparse(uri['img_src']).query
 
         return uris
