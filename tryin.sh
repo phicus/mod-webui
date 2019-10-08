db9412d732899e7f29d1eb54e58d22bad8a10e3b
johnbarneta <jgomez@ideosoft.es>
2019-10-08 13:31:45 +0200

    up

--- tryin.sh
+++ tryin.sh
@@ -9,7 +9,7 @@ fi
 rsync -av --cvs-exclude ./module/$subpath ${xyz}.phicus.es:/var/lib/shinken/modules/webui2/$subpath;
 rsync -av --cvs-exclude ../webui-plugins/* ${xyz}.phicus.es:/var/lib/shinken/modules/webui2/plugins/;
 
-# ssh ${xyz}.phicus.es "service shinken-broker restart; service shinken reload"
+ssh ${xyz}.phicus.es "service shinken-broker restart; service shinken reload"
 
 
 
